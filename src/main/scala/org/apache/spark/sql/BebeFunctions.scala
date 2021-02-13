fdc992a6b8cf8fc3f96044438e04d349ac4965d1
Nik Vanderhoof <nikolasrvanderhoof@gmail.com>
2021-02-13 00:18:28 -0500

    Add approx_percentile function

--- src/main/scala/org/apache/spark/sql/BebeFunctions.scala
+++ src/main/scala/org/apache/spark/sql/BebeFunctions.scala
@@ -1,12 +1,51 @@
 package org.apache.spark.sql
 
 import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.catalyst.expressions.aggregate._
 
 /**
  * @groupname string_funcs String Functions
+ * @groupname agg_funcs Aggregate Functions
  */
 object BebeFunctions {
   private def withExpr(expr: Expression): Column = Column(expr)
+  private def withAggregateFunction(
+      func: AggregateFunction,
+      isDistinct: Boolean = false): Column = {
+    Column(func.toAggregateExpression(isDistinct))
+  }
+
+  /**
+   * Returns the approximate percentile value of numeric
+   * column `col` at the given percentage. The value of percentage must be between 0.0
+   *  and 1.0. The `accuracy` parameter (default: 10000) is a positive numeric literal which
+   *  controls approximation accuracy at the cost of memory. Higher value of `accuracy` yields
+   *  better accuracy, `1.0/accuracy` is the relative error of the approximation.
+   *  When `percentage` is an array, each value of the percentage array must be between 0.0 and 1.0.
+   *  In this case, returns the approximate percentile array of column `col` at the given
+   *  percentage array.
+   *
+   *  @group agg_funcs
+   */
+  def approx_percentile(column: Column, percentage: Column, accuracy: Column): Column =
+    withAggregateFunction {
+      new ApproximatePercentile(column.expr, percentage.expr, accuracy.expr)
+    }
+
+  /**
+   * Returns the approximate percentile value of numeric
+   * column `col` at the given percentage. The value of percentage must be between 0.0
+   *  and 1.0. 
+   *  When `percentage` is an array, each value of the percentage array must be between 0.0 and 1.0.
+   *  In this case, returns the approximate percentile array of column `col` at the given
+   *  percentage array.
+   *
+   *  @group agg_funcs
+   */
+  def approx_percentile(column: Column, percentage: Column): Column =
+    withAggregateFunction {
+      new ApproximatePercentile(column.expr, percentage.expr)
+    }
 
   /**
    * Extract all strings in the `str` that match the `regexp` expression
---
+++ src/test/scala/org/apache/spark/sql/ApproxPercentileSpec.scala
@@ -0,0 +1,249 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Derived from: https://github.com/apache/spark/blob/v3.1.0/sql/core/src/test/scala/org/apache/spark/sql/ApproximatePercentileQuerySuite.scala
+ */
+package org.apache.spark.sql.bebe
+
+import java.sql.{Date, Timestamp}
+
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.BebeFunctions.approx_percentile
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.catalyst.expressions.aggregate.ApproximatePercentile
+import org.apache.spark.sql.catalyst.expressions.aggregate.ApproximatePercentile.DEFAULT_PERCENTILE_ACCURACY
+import org.apache.spark.sql.catalyst.expressions.aggregate.ApproximatePercentile.PercentileDigest
+import org.apache.spark.sql.catalyst.util.DateTimeUtils
+import org.apache.spark.sql.functions._
+import org.scalatest.FunSuite
+
+import com.github.mrpowers.spark.fast.tests.DatasetComparer
+import mrpowers.bebe.SparkSessionTestWrapper
+
+class ApproxPercentileSpec
+    extends FunSuite
+    with SparkSessionTestWrapper {
+
+  private def checkAnswer(df: DataFrame, rows: Seq[Row]): Unit = {
+    assert(df.collect.toSeq == rows)
+  }
+
+  private def checkAnswer(df: DataFrame, row: Row): Unit =
+    checkAnswer(df, Seq(row))
+
+  private def withTempView[T](views: String*)(body: => T) =
+    try body finally {
+      views.foreach(viewname => spark.sql(s"drop view if exists $viewname"))
+    }
+
+  import spark.implicits._
+
+  private val table = "percentile_test"
+
+  test("approx_percentile, single percentile value") {
+    val table = (1 to 1000).toDF("col")
+    checkAnswer(
+      table.select(
+        approx_percentile(col("col"), lit(0.25)),
+        approx_percentile(col("col"), lit(0.5)),
+        approx_percentile(col("col"), lit(0.75d)),
+        approx_percentile(col("col"), lit(0.0)),
+        approx_percentile(col("col"), lit(1.0)),
+        approx_percentile(col("col"), lit(0)),
+        approx_percentile(col("col"), lit(1))
+      ),
+      Row(250D, 500D, 750D, 1D, 1000D, 1D, 1000D)
+    )
+  }
+
+  test("approx_percentile, the first element satisfies small percentages") {
+    val table = (1 to 10).toDF("col")
+    checkAnswer(
+      table
+        .select(approx_percentile(col("col"), array(lit(0.01), lit(0.1), lit(0.11)))),
+      Row(Seq(1, 1, 2))
+    )
+  }
+
+  test("approx_percentile, array of percentile value") {
+    val table = (1 to 1000).toDF("col")
+    checkAnswer(
+      table
+        .select(
+          approx_percentile(col("col"), array(lit(0.25), lit(0.5), lit(0.75D))),
+          count(col("col")),
+          approx_percentile(col("col"), array(lit(0.0), lit(1.0))),
+          sum(col("col"))
+        ),
+      Row(Seq(250D, 500D, 750D), 1000, Seq(1D, 1000D), 500500)
+    )
+  }
+
+  test("approx_percentile, different column types") {
+    val intSeq = 1 to 1000
+    val data: Seq[(java.math.BigDecimal, Date, Timestamp)] = intSeq.map { i =>
+      (new java.math.BigDecimal(i), DateTimeUtils.toJavaDate(i), DateTimeUtils.toJavaTimestamp(i))
+    }
+    val table = data.toDF("cdecimal", "cdate", "ctimestamp")
+    checkAnswer(
+      table
+        .select(
+          approx_percentile(col("cdecimal"), array(lit(0.25), lit(0.5), lit(0.75D))),
+          approx_percentile(col("cdate"), array(lit(0.25), lit(0.5), lit(0.75D))),
+          approx_percentile(col("ctimestamp"), array(lit(0.25), lit(0.5), lit(0.75D)))
+        ),
+      Row(
+        Seq("250.000000000000000000", "500.000000000000000000", "750.000000000000000000")
+            .map(i => new java.math.BigDecimal(i)),
+        Seq(250, 500, 750).map(DateTimeUtils.toJavaDate),
+        Seq(250, 500, 750).map(i => DateTimeUtils.toJavaTimestamp(i.toLong)))
+    )
+  }
+
+  test("approx_percentile, multiple records with the minimum value in a partition") {
+    val table = spark.sparkContext.makeRDD(Seq(1, 1, 2, 1, 1, 3, 1, 1, 4, 1, 1, 5), 4).toDF("col")
+    checkAnswer(
+      table.select(approx_percentile(col("col"), array(lit(0.5)))),
+      Row(Seq(1.0D))
+    )
+  }
+
+  test("approx_percentile, with different accuracies") {
+    val tableCount = 1000
+    val table = (1 to tableCount).toDF("col")
+
+    // With different accuracies
+    val accuracies = Array(1, 10, 100, 1000, 10000)
+    val expectedPercentiles = Array(100D, 200D, 250D, 314D, 777D)
+    for (accuracy <- accuracies) {
+      for (expectedPercentile <- expectedPercentiles) {
+        val df = table
+          .select(
+            approx_percentile(
+              col("col"), lit(expectedPercentile)/lit(tableCount), lit(accuracy)
+            )
+           )
+        val approximatePercentile = df.collect().head.getInt(0)
+        val error = Math.abs(approximatePercentile - expectedPercentile)
+        assert(error <= math.floor(tableCount.toDouble / accuracy.toDouble))
+      }
+    }
+  }
+
+  test("approx_percentile(), aggregation on empty input table, no group by") {
+    val table = Seq.empty[Int].toDF("col")
+    checkAnswer(
+      table.select(sum(col("col")), approx_percentile(col("col"), lit(0.5))),
+      Row(null, null)
+    )
+  }
+
+  test("approx_percentile(), aggregation on empty input table, with group by") {
+    val table = Seq.empty[Int].toDF("col")
+    checkAnswer(
+      table.groupBy("col").agg(sum(col("col")), approx_percentile(col("col"), lit(0.5))),
+      Seq.empty[Row]
+    )
+  }
+
+  test("approx_percentile(null), aggregation with group by") {
+    val table = (1 to 1000).map(x => (x % 3, x)).toDF("key", "value")
+    checkAnswer(
+      table
+        .groupBy("key")
+        .agg(approx_percentile(lit(null), lit(0.5))),
+      Seq(
+        Row(1, null),
+        Row(2, null),
+        Row(0, null))
+    )
+  }
+
+  test("approx_percentile(null), aggregation without group by") {
+    val table = (1 to 1000).map(x => (x % 3, x)).toDF("key", "value")
+    checkAnswer(
+      table.select(
+        approx_percentile(lit(null), lit(0.5)),
+        sum(lit(null)),
+        approx_percentile(lit(null), lit(0.5))
+      ),
+      Row(null, null, null)
+    )
+  }
+
+  test("approx_percentile(col, ...), input rows contains null, with out group by") {
+    val table = (1 to 1000).map(Integer.valueOf(_)).flatMap(Seq(null: Integer, _)).toDF("col")
+    checkAnswer(
+      table.select(
+        approx_percentile(col("col"), lit(0.5)),
+        sum(lit(null)),
+        approx_percentile(col("col"), lit(0.5))
+      ),
+      Row(500D, null, 500D))
+  }
+
+  test("approx_percentile(col, ...), input rows contains null, with group by") {
+    val rand = new java.util.Random()
+    val table = (1 to 1000)
+      .map(Integer.valueOf(_))
+      .map(v => (Integer.valueOf(v % 2), v))
+      // Add some nulls
+      .flatMap(Seq(_, (null: Integer, null: Integer)))
+      .toDF("key", "value")
+    checkAnswer(
+      table.groupBy("key")
+        .agg(
+          approx_percentile(col("value"), lit(0.5)),
+          sum(col("value")),
+          approx_percentile(col("value"), lit(0.5))
+        ).drop("key"),
+      Seq(
+        Row(null, null, null),
+        Row(499.0D, 250000, 499.0D),
+        Row(500.0D, 250500, 500.0D)
+      )
+    )
+  }
+
+  test("approx_percentile(col, ...) works in window function") {
+    val data = (1 to 10).map(v => (v % 2, v))
+    val table = data.toDF("key", "value")
+
+    val windowSpec = Window
+      .partitionBy(col("key"))
+      .orderBy(col("value"))
+      .rowsBetween(Window.unboundedPreceding, 0)
+    val query = table
+      .select(approx_percentile(col("value"), lit(0.5)).over(windowSpec))
+
+    val expected = data.groupBy(_._1).toSeq.flatMap { group =>
+      val (key, values) = group
+      val sortedValues = values.map(_._2).sorted
+
+      var outputRows = Seq.empty[Row]
+      var i = 0
+
+      val percentile = new PercentileDigest(1.0 / DEFAULT_PERCENTILE_ACCURACY)
+      sortedValues.foreach { value =>
+        percentile.add(value)
+        outputRows :+= Row(percentile.getPercentiles(Array(0.5D)).head)
+      }
+      outputRows
+    }
+
+    checkAnswer(query, expected)
+  }
+}
