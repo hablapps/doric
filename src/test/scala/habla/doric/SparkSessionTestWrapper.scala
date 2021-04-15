69a799ddef5e56000e009988d54b0ff61c520afc
Alfonso Roa <alfonso.roa@habla.dev>
2021-04-15 11:00:18 +0200

    Refactor using a kleisly

--- src/test/scala/habla/doric/SparkSessionTestWrapper.scala
+++ src/test/scala/habla/doric/SparkSessionTestWrapper.scala
@@ -1,13 +1,18 @@
 package habla.doric
 
 import org.apache.log4j.{Level, Logger}
+
 import org.apache.spark.sql.SparkSession
 
 trait SparkSessionTestWrapper {
 
   lazy val spark: SparkSession = {
     Logger.getLogger("org").setLevel(Level.OFF)
-    SparkSession.builder().master("local").appName("spark session").getOrCreate()
+    SparkSession
+      .builder()
+      .master("local")
+      .config("spark.driver.bindAddress", "127.0.0.1")
+      .appName("spark session").getOrCreate()
   }
 
 }
