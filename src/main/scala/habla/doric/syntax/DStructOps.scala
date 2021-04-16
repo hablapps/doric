7c4b56f77ed4969eda087b6c6a7a9b55ef1b3819
Alfonso Roa <alfonso.roa@habla.dev>
2021-04-16 11:22:52 +0200

    Struct
    Struct
    
    Struct

--- src/main/scala/habla/doric/syntax/DStructOps.scala
+++ src/main/scala/habla/doric/syntax/DStructOps.scala
@@ -1,16 +1,51 @@
 package habla.doric
 package syntax
 
-import cats.data.NonEmptyChain
+import cats.arrow.FunctionK
+import cats.data.{Kleisli, NonEmptyChain}
+import cats.implicits._
+
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.types.StructType
 
 trait DStructOps {
 
   type DoricEither[A] = Either[NonEmptyChain[Throwable], A]
 
+  private val toValidated = new FunctionK[DoricEither, DoricValidated] {
+    override def apply[A](fa: DoricEither[A]): DoricValidated[A] = fa.toValidated
+  }
+  private val toEither = new FunctionK[DoricValidated, DoricEither] {
+    override def apply[A](fa: DoricValidated[A]): DoricEither[A] = fa.toEither
+  }
+
   implicit class DStructSyntax(private val col: DStructColumn) {
     def getChild[T: FromDf](subColumnName: String): DoricColumn[T] = {
-      val subColumnCompleteName: List[String] = col.name :+ subColumnName
-      get[T](subColumnCompleteName.mkString(".")).copy(name = subColumnCompleteName)
+      col.elem
+        .mapK(toEither)
+        .flatMap(vcolumn =>
+          Kleisli[DoricEither, DataFrame, Column]((df: DataFrame) => {
+            val fatherColumn = df.select(vcolumn).schema.head
+            val fatherStructType = fatherColumn.dataType
+              .asInstanceOf[StructType]
+            fatherStructType
+              .find(_.name == subColumnName)
+              .fold[DoricEither[Column]](
+                new Exception(
+                  s"No such struct field $subColumnName in ${fatherStructType.names.mkString(", ")}"
+                ).leftNec
+              )(st =>
+                if (FromDf[T].isValid(st.dataType))
+                  vcolumn.getItem(subColumnName).asRight[NonEmptyChain[Throwable]]
+                else
+                  new Exception(
+                    s"The nested column $subColumnName is of type ${st.dataType} and it was expected to be ${FromDf[T].dataType}"
+                  ).leftNec[Column]
+              )
+          })
+        )
+        .mapK(toValidated)
+        .toDC
     }
   }
 
