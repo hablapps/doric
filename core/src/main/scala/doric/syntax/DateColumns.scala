2b291a380ece6509e145d2ef2ffc00ec5cf85087
Eduardo Ruiz Alonso <eruizalo@users.noreply.github.com>
2021-10-26 09:47:52 +0200

    #65 Date time functions (#97)
    * feat: add Date & Timestamp functions

--- core/src/main/scala/doric/syntax/DateColumns.scala
+++ core/src/main/scala/doric/syntax/DateColumns.scala
@@ -2,37 +2,241 @@ package doric
 package syntax
 
 import cats.implicits._
-import doric.types.DateType
+import doric.types.{DateType, SparkType}
+import org.apache.spark.sql.catalyst.expressions.{DateFormatClass, MonthsBetween, NextDay, TruncDate}
+import org.apache.spark.sql.{Column, functions => f}
 
-import org.apache.spark.sql.functions
+import java.sql.Date
 
 private[syntax] trait DateColumns {
-  implicit class DateColumnLikeSyntax[T: DateType](
+
+  /**
+    * Returns the current date at the start of query evaluation as a date column.
+    * All calls of current_date within the same query return the same value.
+    *
+    * @group Date Type
+    */
+  def currentDate(): DateColumn = f.current_date().asDoric[Date]
+
+  implicit class DateColumnLikeSyntax[T: DateType: SparkType](
       column: DoricColumn[T]
   ) {
 
     /**
-      * Sets the moment to the last day of the same month.
-      * @group Date Type
-      */
-    def endOfMonth: DoricColumn[T] = column.elem.map(functions.last_day).toDC
-
-    /**
-      * @return
-      *   an Integer DoricColumn with the day number of the date.
-      * @group Date Type
-      */
-    def dayOfMonth: IntegerColumn = column.elem.map(functions.dayofmonth).toDC
-
-    /**
-      * Adds to the date the number of months
-      * @group Date Type
+      * Adds to the Date or Timestamp column the number of months
+      *
+      * @group Date & Timestamp Type
       * @param nMonths
-      *   the number of months to add, can be negative to substract.
+      *   the number of months to add, can be negative to subtract.
       * @return
-      *   DoricColumn of the same type as the original with the month changed.
+      *   Date column after adding months
+      * @note Timestamp columns will be truncated to Date column
       */
-    def addMonths(nMonths: IntegerColumn): DoricColumn[T] =
-      (column.elem, nMonths.elem).mapN(functions.add_months).toDC
+    def addMonths(nMonths: IntegerColumn): DateColumn =
+      (column.elem, nMonths.elem).mapN(f.add_months).toDC
+
+    /**
+      * Returns the date that is `days` days after date column
+      *
+      * @param days A column of the number of days to add to date column, can be negative to subtract days
+      * @group Date & Timestamp Type
+      * @note Timestamp columns will be truncated to Date column
+      */
+    def addDays(days: IntegerColumn): DateColumn =
+      (column.elem, days.elem).mapN(f.date_add).toDC
+
+    /**
+      * Converts a date to a value of string in the format specified by the date
+      * format given by the second argument.
+      *
+      * @param format A pattern `dd.MM.yyyy` would return a string like `18.03.1993`
+      * @note Use specialized functions like 'year' whenever possible as they benefit from a
+      *       specialized implementation.
+      * @group Date & Timestamp Type
+      */
+    def format(format: StringColumn): StringColumn =
+      (column.elem, format.elem)
+        .mapN((c, fmt) => {
+          new Column(DateFormatClass(c.expr, fmt.expr))
+        })
+        .toDC
+
+    /**
+      * Returns the date that is `days` days before date column
+      *
+      * @param days A column of the number of days to subtract from date column, can be negative to add
+      *             days
+      * @group Date & Timestamp Type
+      * @note Timestamp columns will be truncated to Date column
+      */
+    def subDays(days: IntegerColumn): DateColumn =
+      (column.elem, days.elem).mapN(f.date_sub).toDC
+
+    /**
+      * Returns the number of days from date column to `dateCol`.
+      *
+      * @param dateCol A Date or Timestamp column
+      * @group Date & Timestamp Type
+      */
+    def diff(dateCol: DoricColumn[T]): IntegerColumn =
+      (column.elem, dateCol.elem)
+        .mapN((end, start) => f.datediff(end, start))
+        .toDC
+
+    /**
+      * Extracts the day of the month as an integer from a given date.
+      *
+      * @group Date & Timestamp Type
+      */
+    def dayOfMonth: IntegerColumn = column.elem.map(f.dayofmonth).toDC
+
+    /**
+      * Extracts the day of the week as an integer from a given date.
+      * Ranges from 1 for a Sunday through to 7 for a Saturday
+      *
+      * @group Date & Timestamp Type
+      */
+    def dayOfWeek: IntegerColumn = column.elem.map(f.dayofweek).toDC
+
+    /**
+      * Extracts the day of the year as an integer from a given date.
+      *
+      * @group Date & Timestamp Type
+      */
+    def dayOfYear: IntegerColumn = column.elem.map(f.dayofyear).toDC
+
+    /**
+      * Sets the moment to the last day of the same month.
+      *
+      * @group Date & Timestamp Type
+      */
+    def endOfMonth: DateColumn = lastDayOfMonth
+
+    /**
+      * Returns the last day of the month which the given date belongs to.
+      * For example, input "2015-07-27" returns "2015-07-31" since July 31 is the last day of the
+      * month in July 2015.
+      *
+      * @group Date & Timestamp Type
+      */
+    def lastDayOfMonth: DateColumn = column.elem.map(f.last_day).toDC
+
+    /**
+      * Extracts the month as an integer from a given date.
+      *
+      * @group Date & Timestamp Type
+      */
+    def month: IntegerColumn = column.elem.map(f.month).toDC
+
+    /**
+      * Returns number of months between dates date column and `dateCol`.
+      *
+      * A whole number is returned if both inputs have the same day of month or both are the last day
+      * of their respective months. Otherwise, the difference is calculated assuming 31 days per month.
+      *
+      * For example:
+      * {{{
+      * Date("2017-11-14").monthsBetween(Date("2017-07-14"))                              // returns 4.0
+      * Date("2017-01-01").monthsBetween(Date("2017-01-10"))                              // returns 0.29032258
+      * Timestamp("2017-06-01 00:00:00").monthsBetween(Timestamp("2017-06-16 12:00:00"))  // returns -0.5
+      * }}}
+      *
+      * @param dateCol Date or Timestamp column
+      * @group Date & Timestamp Type
+      */
+    def monthsBetween(dateCol: DoricColumn[T]): DoubleColumn =
+      (column.elem, dateCol.elem).mapN(f.months_between).toDC
+
+    /**
+      * Returns number of months between dates `dateCol` and date column.
+      *
+      * @param dateCol Date or Timestamp column
+      * @param roundOff If `roundOff` is set to true, the result is rounded off to 8 digits;
+      *                 it is not rounded otherwise.
+      * @group Date & Timestamp Type
+      */
+    def monthsBetween(
+        dateCol: DoricColumn[T],
+        roundOff: BooleanColumn
+    ): DoubleColumn =
+      (column.elem, dateCol.elem, roundOff.elem)
+        .mapN((c, d, r) => {
+          new Column(new MonthsBetween(c.expr, d.expr, r.expr))
+        })
+        .toDC
+
+    /**
+      * Returns the first date which is later than the value of the `date` column that is on the
+      * specified day of the week.
+      *
+      * For example, `Date("2015-07-27").nextDay("Sunday")` returns Date("2015-08-02") because
+      * that is the first Sunday after 2015-07-27.
+      *
+      * @param dayOfWeek Case insensitive, and accepts: "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+      * @group Date & Timestamp Type
+      * @note Timestamp columns will be truncated to Date column
+      */
+    def nextDay(dayOfWeek: StringColumn): DateColumn =
+      (column.elem, dayOfWeek.elem)
+        .mapN((c, dow) => {
+          new Column(NextDay(c.expr, dow.expr))
+        })
+        .toDC
+
+    /**
+      * Extracts the quarter as an integer from a given date.
+      *
+      * @group Date & Timestamp Type
+      */
+    def quarter: IntegerColumn = column.elem.map(f.quarter).toDC
+
+    /**
+      * Returns date truncated to the unit specified by the format.
+      *
+      * For example, `Timestamp("2018-11-19 12:01:19").trunc("year")` returns Date("2018-01-01")
+      *
+      * @param format 'year', 'yyyy', 'yy' to truncate by year,
+      *               or 'month', 'mon', 'mm' to truncate by month
+      *               Other options are: 'week', 'quarter'
+      * @group Date & Timestamp Type
+      * @note Timestamp columns will be truncated to Date column
+      */
+    def trunc(format: StringColumn): DateColumn =
+      (column.elem, format.elem)
+        .mapN((c, fmt) => {
+          new Column(TruncDate(c.expr, fmt.expr))
+        })
+        .toDC
+
+    /**
+      * Extracts the week number as an integer from a given date.
+      *
+      * A week is considered to start on a Monday and week 1 is the first week with more than 3 days,
+      * as defined by ISO 8601
+      *
+      * @group Date & Timestamp Type
+      */
+    def weekOfYear: IntegerColumn = column.elem.map(f.weekofyear).toDC
+
+    /**
+      * Extracts the year as an integer from a given date.
+      *
+      * @group Date & Timestamp Type
+      */
+    def year: IntegerColumn = column.elem.map(f.year).toDC
+
+    /**
+      * Transform date to timestamp
+      *
+      * @group Date Type
+      */
+    def toTimestamp: TimestampColumn = column.elem.map(f.to_timestamp).toDC
+
+    /**
+      * Transform date to Instant
+      *
+      * @group Date Type
+      */
+    def toInstant: InstantColumn = column.elem.map(f.to_timestamp).toDC
   }
 }
--- core/src/main/scala/doric/syntax/TimestampColumns.scala
+++ core/src/main/scala/doric/syntax/TimestampColumns.scala
@@ -2,33 +2,86 @@ package doric
 package syntax
 
 import doric.types.TimestampType
+import org.apache.spark.sql.{functions => f}
 
-import org.apache.spark.sql.functions
+import java.sql.Timestamp
 
 private[syntax] trait TimestampColumns {
+
+  /**
+    * Returns the current timestamp at the start of query evaluation as a timestamp column.
+    * All calls of current_timestamp within the same query return the same value.
+    *
+    * @group Timestamp Type
+    */
+  def currentTimestamp(): TimestampColumn =
+    f.current_timestamp().asDoric[Timestamp]
+
   implicit class TimestampColumnLikeSyntax[T: TimestampType](
       column: DoricColumn[T]
   ) {
 
     /**
+      * Extracts the seconds as an integer from a given timestamp.
+      *
       * @group Timestamp Type
-      * @return
-      *   the hour of the time.
       */
-    def hour: IntegerColumn = column.elem.map(functions.hour).toDC
+    def second: IntegerColumn = column.elem.map(f.second).toDC
+
+    /**
+      * Generates tumbling time windows given a timestamp specifying column. Window
+      * starts are inclusive but the window ends are exclusive.
+      *
+      * @param windowDuration A string specifying the width of the window, e.g. `10 minutes`,
+      *                       `1 second`. Check `org.apache.spark.unsafe.types.CalendarInterval` for
+      *                       valid duration identifiers.
+      * @group Timestamp Type
+      */
+    def window(windowDuration: String): DStructColumn =
+      column.elem.map(x => f.window(x, windowDuration)).toDC
+
+    /**
+      * Generates tumbling time windows given a timestamp specifying column. Window
+      * starts are inclusive but the window ends are exclusive.
+      *
+      * @param windowDuration A string specifying the width of the window, e.g. `10 minutes`,
+      *                       `1 second`. Check `org.apache.spark.unsafe.types.CalendarInterval` for
+      *                       valid duration identifiers. Note that the duration is a fixed length of
+      *                       time, and does not vary over time according to a calendar. For example,
+      *                       `1 day` always means 86,400,000 milliseconds, not a calendar day.
+      * @param slideDuration A string specifying the sliding interval of the window, e.g. `1 minute`.
+      *                      A new window will be generated every `slideDuration`. Must be less than
+      *                      or equal to the `windowDuration`. Check
+      *                      `org.apache.spark.unsafe.types.CalendarInterval` for valid duration
+      *                      identifiers. This duration is likewise absolute, and does not vary
+      *                      according to a calendar.
+      * @param startTime The offset with respect to 1970-01-01 00:00:00 UTC with which to start
+      *                  window intervals. For example, in order to have hourly tumbling windows that
+      *                  start 15 minutes past the hour, e.g. 12:15-13:15, 13:15-14:15... provide
+      *                  `startTime` as `15 minutes`.
+      * @group Timestamp Type
+      */
+    def window(
+        windowDuration: String,
+        slideDuration: String,
+        startTime: String = "0 second"
+    ): DStructColumn =
+      column.elem
+        .map(x => f.window(x, windowDuration, slideDuration, startTime))
+        .toDC
 
     /**
       * @group Timestamp Type
       * @return
       *   a Date Column without the hour
       */
-    def toDate: DateColumn = column.elem.map(functions.to_date).toDC
+    def toDate: DateColumn = column.elem.map(f.to_date).toDC
 
     /**
       * @group Timestamp Type
       * @return
       *   a LocalDate Column without the hour
       */
-    def toLocalDate: LocalDateColumn = column.elem.map(functions.to_date).toDC
+    def toLocalDate: LocalDateColumn = column.elem.map(f.to_date).toDC
   }
 }
