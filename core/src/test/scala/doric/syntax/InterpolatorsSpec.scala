fb3aed3e25ee8040b17ffb6fed5534794528cd90
Eduardo Ruiz <eduardo.ruiz@hablapps.com>
2022-02-05 11:20:17 +0100

    feat: [+] StringColumn interpolator (#165)

---
+++ core/src/test/scala/doric/syntax/InterpolatorsSpec.scala
@@ -0,0 +1,39 @@
+package doric
+package syntax
+
+import org.scalatest.EitherValues
+import org.scalatest.matchers.should.Matchers
+
+import org.apache.spark.sql.{functions => f}
+
+class InterpolatorsSpec
+    extends DoricTestElements
+    with EitherValues
+    with Matchers {
+
+  describe("String interpolator") {
+    import spark.implicits._
+
+    val colName = "column1"
+    val df = List("value 1", "value 2")
+      .toDF(colName)
+
+    it("should be a String Column") {
+      df.validateColumnType[String](ds"column")
+      df.validateColumnType[String](ds"column ${colString(colName)}")
+      df.validateColumnType[String](ds"${colString(colName)}")
+    }
+
+    it("should concat literals and columns") {
+      df.testColumns2(colName, "Column has value:")(
+        (c, str) => ds"${str.lit} -->${colString(c)}",
+        (c, str) => f.concat(f.lit(str), f.lit(" -->"), f.col(c)),
+        List(
+          Some("Column has value: -->value 1"),
+          Some("Column has value: -->value 2")
+        )
+      )
+    }
+  }
+
+}
