a74e0785a083bc529977b44731cd16b413f10f9b
Eduardo Ruiz Alonso <eduardo.ruiz@hablapps.com>
2022-03-29 09:51:10 +0200

    fix: [~] str interpolator now prints empty if null column (issue #191) (#199)

--- core/src/main/scala/doric/syntax/Interpolators.scala
+++ core/src/main/scala/doric/syntax/Interpolators.scala
@@ -4,15 +4,15 @@ package syntax
 trait Interpolators {
   implicit class doricStringInterpolator(val sc: StringContext) {
     def ds(columns: StringColumn*): StringColumn = {
-      val literals: Seq[DoricColumn[String]] = sc.parts.map(_.lit)
+      val literals: Seq[StringColumn] = sc.parts.map(_.lit)
 
-      val cols: Seq[DoricColumn[String]] = literals
+      val cols: Seq[StringColumn] = literals
         .zipAll(columns, "".lit, "".lit)
         .flatMap { case (a, b) =>
           List(a, b)
         }
 
-      concat(cols: _*)
+      concatWs("".lit, cols: _*)
     }
   }
 }
