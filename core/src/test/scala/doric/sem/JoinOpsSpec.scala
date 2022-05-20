74917fcec4e8989313a07e2be866291d1a7b0d59
Alfonso Roa <alfonso.roa@habla.dev>
2022-05-20 15:28:51 +0200

    Update JoinOpsSpec.scala

--- core/src/test/scala/doric/sem/JoinOpsSpec.scala
+++ core/src/test/scala/doric/sem/JoinOpsSpec.scala
@@ -7,8 +7,6 @@ import org.scalatest.EitherValues
 
 class JoinOpsSpec extends DoricTestElements with Matchers with EitherValues {
 
-  import doric._
-
   private val id          = "id"
   private val otherColumn = "otherColumn"
   private val left = spark
