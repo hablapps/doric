956777f633c11d1d323dc079f1fcb6edb6809958
Eduardo Ruiz <eduardo.ruiz@hablapps.com>
2022-12-30 16:04:56 +0100

    feat: [+] #307 Add spark 3.3.1

--- core/src/test/scala/doric/sem/SortingOpsSpec.scala
+++ core/src/test/scala/doric/sem/SortingOpsSpec.scala
@@ -1,7 +1,7 @@
 package doric.sem
 
 import doric.{DoricTestElements, colInt, colString}
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{functions => f, Row}
 
 class SortingOpsSpec extends DoricTestElements {
   import spark.implicits._
@@ -28,25 +28,25 @@ class SortingOpsSpec extends DoricTestElements {
 
   describe("Sort Within Partitions") {
     it("sorts dataframe partitions with sort function on one column") {
-      val df = List((1, "a"), (2, "b"), (3, "c"), (4, "d"))
+      val df = List((2, "a"), (1, "a"), (3, "b"), (4, "b"))
         .toDF("col1", "col2")
-        .repartition(2)
+        .repartition(2, f.col("col2"))
 
       val res    = df.sortWithinPartitions(colInt("col1").asc)
-      val actual = List(Row(3, "c"), Row(4, "d"), Row(1, "a"), Row(2, "b"))
+      val actual = List(Row(1, "a"), Row(2, "a"), Row(3, "b"), Row(4, "b"))
 
       res.collect().toList should contain theSameElementsInOrderAs actual
     }
 
     it("sorts dataframe partitions with sort function on multiple columns") {
-      val df = List((1, "z"), (2, "n"), (3, "x"), (2, "f"), (2, "z"))
+      val df = List((1, "a"), (2, "a"), (3, "a"), (1, "b"), (2, "b"))
         .toDF("col1", "col2")
-        .repartition(2)
+        .repartition(2, f.col("col2"))
 
       val res =
         df.sortWithinPartitions(colInt("col1").desc, colString("col2").asc)
       val actual =
-        List(Row(2, "n"), Row(2, "z"), Row(1, "z"), Row(3, "x"), Row(2, "f"))
+        List(Row(3, "a"), Row(2, "a"), Row(1, "a"), Row(2, "b"), Row(1, "b"))
 
       res.collect().toList should contain theSameElementsInOrderAs actual
     }
