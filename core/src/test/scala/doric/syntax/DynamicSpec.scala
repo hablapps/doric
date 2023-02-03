1fd8884f7c05428573c3aba04276604cff597e8a
Eduardo Ruiz Alonso <eduardo.ruiz@hablapps.com>
2023-02-03 13:40:10 +0100

    fix: [~] child dynamic accessor only for struct columns (issue #265) (#269)

--- core/src/test/scala/doric/syntax/DynamicSpec.scala
+++ core/src/test/scala/doric/syntax/DynamicSpec.scala
@@ -16,26 +16,29 @@ class DynamicSpec extends DoricTestElements with EitherValues with Matchers {
   describe("Dynamic invocations") {
 
     it("can get values from sub-columns of `Row`` columns") {
-      df.validateColumnType(colStruct("user").name[String])
-      df.validateColumnType(colStruct("user").age[Int])
+      colStruct("user").child.name[String]
+      df.validateColumnType(colStruct("user").child.name[String])
+      df.validateColumnType(colStruct("user").child.age[Int])
     }
 
     it("can get values from sub-sub-columns") {
       List(((("1", 2.0), 2), true))
         .toDF()
-        .validateColumnType(colStruct("_1")._1[Row]._1[String])
+        .validateColumnType(colStruct("_1").child._1[Row].child._1[String])
     }
 
     it("can get values from the top-level row") {
       df.validateColumnType(row.user[Row])
-      df.validateColumnType(row.user[Row].age[Int])
+      df.validateColumnType(row.user[Row].child.age[Int])
       List(("1", 2, true)).toDF().validateColumnType(row._1[String])
-      List((("1", 2), true)).toDF().validateColumnType(row._1[Row]._2[Int])
+      List((("1", 2), true))
+        .toDF()
+        .validateColumnType(row._1[Row].child._2[Int])
     }
 
     if (minorScalaVersion >= 12)
       it("should not compile if the parent column is not a row") {
-        """val c: DoricColumn[String] = col[Int]("id").name[String]""" shouldNot compile
+        """val c: DoricColumn[String] = col[Int]("id").child.name[String]""" shouldNot compile
       }
   }
 }
